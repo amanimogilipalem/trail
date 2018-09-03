8c5608238a51c97a363d9e1ef8c3f6c9e92ac11a
amanimogilipalem <40632572+amanimogilipalem@users.noreply.github.com>
2018-09-03 09:31:45 +0530

    build_maven files

---
+++ App.java
@@ -0,0 +1,13 @@
+package ex_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
